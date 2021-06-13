5764e3f36299118492d8b972376a5c0c5fd88b38
Manuel Kpatsu <manuelkpatsu@gmail.com>
2021-06-13 18:46:58 +0000

    Add a side drawer

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../widgets/app_drawer.dart';
 import '../providers/cart.dart';
 import '../widgets/badge.dart';
-
 import '../widgets/products_grid.dart';
 import 'cart_screen.dart';
 
@@ -64,6 +64,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           ),
         ],
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showFavoritesOnly),
     );
   }
